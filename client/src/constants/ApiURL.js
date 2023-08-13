6c1adff6cabbb5b1bb342fbf5f212839a0c5d722
Sunlight0757 <sunlight0757@gmail.com>
2023-08-13 08:08:32 +0300

    update proxy

--- client/src/constants/ApiURL.js
+++ client/src/constants/ApiURL.js
@@ -1,3 +1,4 @@
-export const SERVER_URL = "http://localhost:5000";
+// export const SERVER_URL = "localhost:5000";
+export const SERVER_URL = "https://tskr-rho.vercel.app:5000";
 
 export const USER_URL = SERVER_URL + "/users";
