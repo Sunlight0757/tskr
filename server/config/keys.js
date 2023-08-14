92651eb9c152e23b4582b1bbe68e5ca270971410
Sunlight0757 <sunlight0757@gmail.com>
2023-08-14 21:42:09 +0300

    mongoDB connect update

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,9 @@
+// module.exports = {
+//   mongoURI: "mongodb://127.0.0.1:27017/tskr",
+//   secretOrKey: "secret",
+// };
 module.exports = {
-  mongoURI: "mongodb://127.0.0.1:27017/tskr",
+  mongoURI:
+    "mongodb+srv://Sunlight0757:M4ZXmiy3AWTDAMev@cluster0.axv2rzz.mongodb.net/",
   secretOrKey: "secret",
 };
