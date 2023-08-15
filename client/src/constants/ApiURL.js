41b51144daf6c4c89e601664fb9491448a67c3bc
Sunlight0757 <sunlight0757@gmail.com>
2023-08-15 19:01:10 +0300

    server url updated

--- client/src/constants/ApiURL.js
+++ client/src/constants/ApiURL.js
@@ -1,4 +1,4 @@
 // export const SERVER_URL = "http://localhost:5000";
-export const SERVER_URL = "https://tskr-4okj.vercel.app";
+export const SERVER_URL = "https://tskr-server.vercel.app";
 
 export const USER_URL = SERVER_URL + "/users";
